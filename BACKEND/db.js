133906367ce4625f2520d48d9b539fc20178eb15
yash <yashgandhi156@gmail.com>
2023-05-25 15:15:01 +0530

    second commit

---
+++ BACKEND/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+// 127.0.0.1
+const mongoURI = "mongodb://127.0.0.1:27017"
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connect to mongo Sucessfully");
+    })
+}
+
+module.exports = connectToMongo;

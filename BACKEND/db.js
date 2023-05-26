07c1c555ac6e952af9c08be9604bd86fbffd1529
yash <yashgandhi156@gmail.com>
2023-05-26 18:03:49 +0530

    hashing password

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // 127.0.0.1
-const mongoURI = "mongodb://127.0.0.1:27017"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
 mongoose.set('strictQuery', false);
 mongoose.set('strictQuery', true);
 
